6a7c24cc6e8e3e118c77add95062d50b784f95ce
Montse Viyuela <montsemsanz@gmail.com>
2022-07-15 18:34:51 +0200

    Modificada clas HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ import java.time.format.DateTimeFormatter;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hola mundo");
         pruebaParsear();
     }
     
